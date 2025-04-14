d8ac0df7292717ba7e85e48422473bde42295225
David Vreony <dpvreony@users.noreply.github.com>
2025-04-14 18:46:43 +0100

    add basic code for ci

---
+++ src/Workflow.UnitTests/SomeRecordTests.cs
@@ -0,0 +1,17 @@
+﻿using Xunit;
+
+namespace Workflow.UnitTests
+{
+    public static class SomeRecordTests
+    {
+        public sealed class ConstructorMethod
+        {
+            [Fact]
+            public void ReturnsInstance()
+            {
+                var instance = new Workflow.SomeRecord(42);
+                Assert.NotNull(instance);
+            }
+        }
+    }
+}
