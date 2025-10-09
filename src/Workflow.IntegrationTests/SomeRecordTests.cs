66a37f0d110d5f504d325aec8479227e7aa493fb
David Vreony <dpvreony@users.noreply.github.com>
2025-10-09 20:53:40 +0100

    add fake int test

---
+++ src/Workflow.IntegrationTests/SomeRecordTests.cs
@@ -0,0 +1,15 @@
+﻿namespace Workflow.IntegrationTests
+{
+    public static class SomeRecordTests
+    {
+        public sealed class ConstructorMethod
+        {
+            [Fact]
+            public void ReturnsInstance()
+            {
+                var instance = new Workflow.SomeRecord(42);
+                Assert.NotNull(instance);
+            }
+        }
+    }
+}
