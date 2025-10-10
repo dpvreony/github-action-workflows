105103acc70af2de657c37327bbd4a9d8986c022
David Vreony <dpvreony@users.noreply.github.com>
2025-10-10 19:35:23 +0100

    housekeeping: add fake int test (#82)
    * add fake int test
    
    * Update SomeRecordTests.cs

---
+++ src/Workflow.IntegrationTests/SomeRecordTests.cs
@@ -0,0 +1,17 @@
+﻿using Xunit;
+
+namespace Workflow.IntegrationTests
+{
+    public static class SomeRecordTests
+    {
+        public sealed class ConstructorMethod
+        {
+            [Fact]
+            public void ReturnsInstance()
+            {
+                var instance = new Workflow.SomeRecord(42);
+                Assert.NotNull(instance);
+            }
+        }
+    }
+}
